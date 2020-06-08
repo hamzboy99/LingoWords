0ad18904843807191105874fb22e75826452e79f
hamza_el9@live.nl <Kam12340>
2020-06-08 18:44:04 +0200

    test

--- src/main/java/bep/lingo/application/WordProcessorInterface.java
+++ src/main/java/bep/lingo/application/WordProcessorInterface.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 import java.util.List;
 
 /**
- *
- *
  * @author Hamza Kayouhi
  */
 
 public interface WordProcessorInterface {
     void storeWords(List<String> content, String file) throws IOException;
+
     List<String> filterWords(List<String> content);
 
 }
