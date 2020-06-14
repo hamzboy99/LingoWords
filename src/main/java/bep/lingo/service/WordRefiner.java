deaa9078e1d32f30354b8202a6ece398076526dd
hamza_el9@live.nl <Kam12340>
2020-06-14 22:31:55 +0200

    optimalisation

--- src/main/java/bep/lingo/service/WordRefiner.java
+++ src/main/java/bep/lingo/service/WordRefiner.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class WordRefiner implements FileDeserializerInterface {
+public class WordRefiner implements WordRefinerInterface {
 
     public WordRefiner() {
     }
